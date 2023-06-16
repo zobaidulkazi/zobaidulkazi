cfde5af8f8ef4c78dea3b4033561ba5e6a071356
zobaidul kazi <mdzobaidulislamkazi@gmail.com>
2023-06-16 15:49:18 +0600

    about apge add

---
+++ app/about/about.layoyt.ts
@@ -0,0 +1,17 @@
+
+export const metadata = {
+  title: 'Welcome About Page in zobaidulkazi',
+  description: 'welcome my about page',
+}
+
+
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    {children}
+  )
+}
