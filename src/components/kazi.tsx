53ae6579d2de27ce826faa961f025e8e78ac5a2f
zobaidulkazi <mdzobaidulislamkazi@gmail.com>
2023-11-27 17:53:54 +0600

    full page upload 01v-03

---
+++ src/components/kazi.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function KaziPage() {
+    const app = () => {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+  return (
+    <div>
+        <h1>Hello Kazi</h1>
+    </div>
+  )
+}
+
+export default KaziPage
