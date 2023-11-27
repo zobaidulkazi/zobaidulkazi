af4c2580e38eb426ce7c61ca27e07dbfbf3ebd55
zobaidulkazi <mdzobaidulislamkazi@gmail.com>
2023-11-27 18:11:17 +0600

    add kazi cp

--- src/components/kazi.tsx
+++ src/components/kazi.tsx
@@ -3,7 +3,8 @@ function Kazi() {
   return (
     <div>
         <h1>@zobaidulkazi</h1>
-    </div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem odio similique eius maxime sequi optio vero voluptate blanditiis, excepturi accusamus natus magni vel. Aliquid beatae, ipsum ullam nam laboriosam commodi?</p>
+            </div>
   )
 }
 
